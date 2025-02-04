b6474df9bd3c76b86e5a8eecba7e1a759e7d0c6b
Jye Harry <jyeharry21@hotmail.com>
2025-02-05 00:27:44 +1100

    Create strapi api fetch wrapper

---
+++ client/src/lib/strapi.ts
@@ -0,0 +1,32 @@
+import qs from "qs";
+
+interface Props {
+  endpoint: string;
+  query?: Record<string, unknown>;
+}
+
+/**
+ * Fetches data from the Strapi API
+ * @param endpoint - The endpoint to fetch from
+ * @param query - The query parameters to add to the url
+ * @returns
+ */
+export default async function fetchApi<T>({
+  endpoint,
+  query,
+}: Props): Promise<T> {
+  if (endpoint.startsWith('/')) {
+    endpoint = endpoint.slice(1);
+  }
+
+  const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`);
+
+  if (query) {
+    const q = qs.stringify(query)
+    url.search = q
+  }
+  const res = await fetch(url.toString());
+  let data = await res.json();
+
+  return data.data as T
+}
