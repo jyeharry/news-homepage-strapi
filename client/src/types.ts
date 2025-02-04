58c18b03c85cc2f14ea61e2af62ac82460b7775e
Jye Harry <jyeharry21@hotmail.com>
2025-02-05 00:28:03 +1100

    Create Article type

---
+++ client/src/types.ts
@@ -0,0 +1,75 @@
+export type Article = {
+  id: number,
+  documentId: string,
+  title: string,
+  preview: string,
+  body: RichText[],
+  createdAt: string,
+  updatedAt: string,
+  publishedAt: string,
+  slug: string,
+  isFeatured: boolean,
+  popularityRank: number | null,
+  displayImage: ResponsiveImage
+}
+
+type MediaImageFormat = {
+  ext: string,
+  url: string,
+  hash: string,
+  mime: string,
+  name: string,
+  path: string | null,
+  size: number,
+  width: number,
+  height: number,
+  sizeInBytes: number
+}
+
+type MediaImageFormatMap = {
+  [key in 'thumbnail' | 'small' | 'medium' | 'large']?: MediaImageFormat
+}
+
+type MediaImage = {
+  id: number,
+  documentId: string,
+  name: string,
+  alternativeText: string | null,
+  caption: string | null,
+  width: number,
+  height: number,
+  formats: MediaImageFormatMap,
+  hash: string,
+  ext: string,
+  mime: string,
+  size: number,
+  url: string,
+  previewUrl: string | null,
+  provider: string,
+  provider_metadata: null,
+  createdAt: string,
+  updatedAt: string,
+  publishedAt: string
+}
+
+type ResponsiveImageSource = {
+  id: number,
+  screenWidth: string,
+  source: MediaImage
+}
+
+type ResponsiveImage = {
+  id: number,
+  sources: ResponsiveImageSource[],
+  default: MediaImage
+}
+
+type RichText = {
+  type: string
+  children: RichTextNode[]
+}
+
+type RichTextNode = {
+  text: string
+  type: string
+}
